86341217572df5f6919a6b2d215eb1c5995045fe
vlalithaunc <vlalitha@email.unc.edu>
2022-04-13 11:32:39 -0400

    updated

--- database.js
+++ database.js
@@ -19,16 +19,16 @@ if (row === undefined) {
 // Set a const that will contain your SQL commands to initialize the database.
     const sqlInit = `
         CREATE TABLE accesslog ( 
-            remoteaddr TEXT, 
-            remoteuser TEXT, 
-            time TEXT, 
-            method TEXT, 
-            url TEXT, 
-            protocol TEXT, 
-            httpversion TEXT, 
-            status TEXT, 
-            referer TEXT,
-            useragent TEXT );
+            remoteaddr VARCHAR, 
+            remoteuser VARCHAR, 
+            time VARCHAR, 
+            method VARCHAR, 
+            url VARCHAR, 
+            protocol VARCHAR, 
+            httpversion NUMERIC, 
+            status INTEGER, 
+            referer VARCHAR,
+            useragent VARCHAR );
     `;
 // Execute SQL commands that we just wrote above.
     db.exec(sqlInit);
