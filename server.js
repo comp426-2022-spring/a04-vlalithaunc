a5695530212f3daaa0ebfb801f7c34d0f55ee042
vlalithaunc <vlalitha@email.unc.edu>
2022-04-14 11:30:13 -0400

    updated

--- server.js
+++ server.js
@@ -2,33 +2,60 @@
 const express = require('express')
 const app = express()
 const fs = require('fs')
-const morgan = require('morgan')
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(express.static('.//frontend'))
-
+//database
 const log_db = require('./database.js')
 
+//allow express to read urlencoded and json
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(express.static('.//frontend'))
+
 const md5 = require('md5')
+//const res = require('express/lib/response')
 
+//require minimist module and 
 const args = require('minimist')(process.argv.slice(2))
-
-// args["port"]
-console.log(args)
+args['port', 'debug', 'log', 'debug']
 const port = args.port || process.env.PORT || 5555;
+
+
+if (args.help == true) {
+    const help = (`
+    server.js [options]
+
+    --port	Set the port number for the server to listen on. Must be an integer
+            between 1 and 65535.
+
+    --debug	If set to true, creates endlpoints /app/log/access/ which returns
+            a JSON access log from the database and /app/error which throws 
+            an error with the message "Error test successful." Defaults to 
+            false.
+
+    --log		If set to false, no log files are written. Defaults to true.
+            Logs are always written to database.
+
+    --help	Return this message and exit.
+    `)
+    // If --help or -h, echo help text to STDOUT and exit
+    console.log(help)
+    process.exit(0)
+}
+
 const server = app.listen(port, () => {
     console.log('App is running on %PORT%'.replace('%PORT%', port))
 })
 
 if (args.log == true) {
     // Use morgan for logging to files
+    const morgan = require('morgan')
     // Create a write stream to append (flags: 'a') to a file
     const accessLog = fs.createWriteStream('access.log', { flags: 'a' })
     // Set up the access logging middleware
     app.use(morgan('combined', { stream: accessLog })) 
 }
 
+//middleware for querying to access log database
 app.use((req, res, next) =>{
     let logdata = {
         remoteaddr: req.ip,
@@ -48,45 +75,6 @@ app.use((req, res, next) =>{
             String(logdata.protocol), String(logdata.httpversion), String(logdata.status), String(logdata.referer), String(logdata.useragent))
     next()
 })
-
-const help = (`
-server.js [options]
-
---port	Set the port number for the server to listen on. Must be an integer
-            between 1 and 65535.
-
---debug	If set to true, creates endlpoints /app/log/access/ which returns
-            a JSON access log from the database and /app/error which throws 
-            an error with the message "Error test successful." Defaults to 
-            false.
-
---log		If set to false, no log files are written. Defaults to true.
-            Logs are always written to database.
-
---help	Return this message and exit.
-`)
-// If --help or -h, echo help text to STDOUT and exit
-
-//middleware for querying to access log database
-
-if (args.debug == true) {
-        app.get("/app/log/access", (req, res) => {
-            try{
-                const stmt = log_db.prepare('SELECT * FROM accesslog').all()
-                res.status(200).json(stmt)
-            } catch(e){
-                console.error(e)
-        }
-        })
-        app.get("/app/error", (req, res) => {
-            console.log("error")
-        })
-}
-
-if (args.help || args.h) {
-    console.log(help)
-    process.exit(0)
-}
 
 //functions from coin.mjs
 function coinFlip() {
@@ -172,6 +160,21 @@ app.get('/app/flip/call/:guess(heads|tails)/', (req, res) =>{
     const game_summary = flipACoin(req.params.guess);
     res.status(200).json(game_summary);
 })
+
+if (args.debug == true) {
+    app.get("/app/log/access", (req, res) => {
+        try{
+            const stmt = log_db.prepare('SELECT * FROM accesslog').all()
+            res.status(200).json(stmt)
+        } catch(e){
+            console.error(e)
+        }
+    });
+    app.get("/app/error", (req, res) => {
+        res.status(500);
+        throw new Error('Error test completed successfully.')
+    })
+}
 
 app.use(function(req, res){
     const statusCode = 404;
